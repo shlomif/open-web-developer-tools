924af9931f25df9f3325576cc3f5047d4dc40a72
Shlomi Fish <shlomif@shlomifish.org>
2018-11-01 23:53:04 +0200

    update jquery-ui

--- open-web-dev-tools/bin/install-npm-deps.sh
+++ open-web-dev-tools/bin/install-npm-deps.sh
@@ -5,7 +5,7 @@
 # Distributed under terms of the MIT license.
 #
 npm install \
-    amdefine babel-cli babel-preset-env babel-preset-react babel-preset-stage-2 big-integer browserify eslint eslint-config-google jsonminify qunit qunit-cli requirejs tslint typescript uglify-es \
+    amdefine babel-cli babel-preset-env babel-preset-react babel-preset-stage-2 big-integer browserify eslint eslint-config-google jsonminify qunit qunit-cli requirejs sass tslint typescript uglify-es \
     @types/jsonminify \
     @types/qunit \
 
